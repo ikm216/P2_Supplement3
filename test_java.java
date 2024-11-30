a8e95deb1b61fd1aed9158351feb5d686e095e0e
ikm216 <ikmclassof22@gmail.com>
2024-11-30 18:49:05 -0500

    test:added test for the binary search

--- test_java.java
+++ test_java.java
@@ -1,3 +1,8 @@
+import org.junit.*;
+
 public class test_java {
-    
+    int [] arr = {2, 4, 6, 8, 10};
+    int search = 8;
+    int test = search.binarySearch(arr, search);
+    Assert.assertEquals(8, test);
 }
