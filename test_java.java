865a9e638d930ac358685879e88f8e3b84e1c02b
ikm216 <ikmclassof22@gmail.com>
2024-11-30 18:51:46 -0500

    test:fixed test for the binary search. Forgot to add the @Test and the void method

--- test_java.java
+++ test_java.java
@@ -1,8 +1,11 @@
 import org.junit.*;
 
 public class test_java {
-    int [] arr = {2, 4, 6, 8, 10};
+    @Test
+    public void testBinarySearchFound(){
+        int [] arr = {2, 4, 6, 8, 10};
     int search = 8;
     int test = search.binarySearch(arr, search);
     Assert.assertEquals(8, test);
+    }
 }
