be18fb247a1e84f02ae0412a12a455ce552fc934
ikm216 <ikmclassof22@gmail.com>
2024-11-30 19:59:11 -0500

    feat:added feature for finding the determinant of the matrix

--- search.java
+++ search.java
@@ -18,5 +18,43 @@ public class search{
             return -1;
         }
     }
-   
+    public static int findDeterminate(int[][] matrix, int N){
+        int d = 0;
+        if(N == 1){
+            return matrix[0][0];
+        }
+        else if(N == 2){
+            return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+        }
+
+        else{
+            int[][] newMatrix = new int [N - 1][];
+            d = 0;
+
+            for (int i = 0; i < N; i++){
+                for (int k = 0; k < (N - 1); k++){
+                    newMatrix[k] = new int[N -1];
+                }
+                for(int j = 1; j < N; j++){
+                    int skip = 0;
+                    for(int l = 0; l < N; l++){
+                        if(l == i)
+                            continue;
+                        newMatrix[j - 1][skip] = matrix[j][l];
+                        skip++;
+                    }
+                }
+                int neg;
+                if(i % 2 == 0){
+                    neg = 1;
+                }
+                else{
+                    neg = -1;
+                }
+
+                d += neg * matrix[0][i] * findDeterminate(newMatrix, N - 1);
+            }
+        }
+        return d;
+    }
 }
