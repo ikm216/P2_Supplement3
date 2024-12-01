dbd9650f3b7e97635b790477bd09f9baee0e7fa0
ikm216 <ikmclassof22@gmail.com>
2024-11-30 20:37:09 -0500

    feat:added feature for multplying matrix

--- search.java
+++ search.java
@@ -84,5 +84,23 @@ public class search{
         return d;
     }
 
-    
+    public static int[][] multiplyMatrix(int[][] matrix1, int[][] matrix2){
+        int row1 = matrix1.length;
+        int col1 = matrix1[0].length;
+        int row2 = matrix2.length;
+        int col2 = matrix2[0].length;
+
+        if (col1 != row2){
+            throw new IllegalArgumentException("Doesn't match");
+        }
+        int answer[][] = new int[row1][col2];
+        for(int i = 0; i < row1; i++){
+            for(int j = 0; j < col2; j++){
+                for(int k = 0; k < col1; k++){
+                    answer[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
 }
