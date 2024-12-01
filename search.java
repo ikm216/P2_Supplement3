198c26eede9b6c4095aba0d98e496a28b31145f7
ikm216 <ikmclassof22@gmail.com>
2024-11-30 19:07:47 -0500

    feat:added feature for the binary search

--- search.java
+++ search.java
@@ -1,3 +1,21 @@
 public class search{
-    p
+    public static int binarySearch(int[] arr, int low, int high, int search){
+        
+        if(arr[low] <= arr[high]){
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == search){
+                return mid;
+            }
+            else if (arr[mid] < search){
+                return binarySearch(arr, mid + 1, high, search);
+            }
+            else{
+                return binarySearch(arr, mid - 1, high, search);
+            }
+        }
+        else{
+            return -1;
+        }
+    }
 }
